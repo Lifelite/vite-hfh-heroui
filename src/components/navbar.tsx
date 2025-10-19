879d3174af1c90b786381657517ed59e435202c4
Lifelite <j.t.huntsman@gmail.com>
2025-10-19 15:18:21 -0400

    fix navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,9 +5,6 @@ import {
   NavbarContent,
   NavbarItem,
 } from "@heroui/navbar";
-import { link as linkStyles } from "@heroui/theme";
-import clsx from "clsx";
-
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { GoFundMeIcon, Venmo } from "@/components/icons";
@@ -25,22 +22,7 @@ export const Navbar = () => {
             <p className="font-bold text-inherit">Hearts For Hannes</p>
           </Link>
         </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </Link>
-            </NavbarItem>
-          ))}
-        </div>
+
       </NavbarContent>
 
       <NavbarContent
